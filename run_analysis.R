ae258a2ff4ed336ba9ba2da7192ff7b056dd8a8f
Holsten <sceaser2000@yahoo.com>
2014-06-20 16:46:27 +0200

    Second commit of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,58 +1,58 @@
-
-library(data.table)
+library(plyr)
+library(reshape2)
 
 # Read test data into R
 xtest <- read.table("./UCI HAR Dataset/test/X_test.txt",header = FALSE)
-ytest <- read.table("./UCI HAR Dataset/test/y_test.txt",header = FALSE)
-subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+ytest <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "Activity_label")
+subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
 
 # Read train data into R.
 xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt",header = FALSE)
-ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt",header = FALSE)
-subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
-
-# Merge x, y and subject data for both train and test datasets.
-xdata <- rbind(xtrain, xtest)
-ydata <- rbind(ytrain, ytest)
-subject <- rbind(subjecttrain, subjecttest)
-
+ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt",col.names = "Activity_label")
+subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
 
 
 # Read features labels into R. 
 features <- read.table("./UCI HAR Dataset/features.txt",)[,"V2"]
 
 
-# Set descriptive names for all columns in xdata, ydata and subject datasets.
-colnames(xdata) <- features
-colnames(ydata) <- c("act_label")
-colnames(subject) <- c("Subject")
+# Set descriptive names for all columns in xtest and xtrain datasets.
+colnames(xtest) <- features
+colnames(xtrain) <- features
 
 
-# Extracts only columns in xdata containing measurements on the mean and standard deviation.
-meanCol <- grep("[Mm]ean", colnames(xdata))
-stdCol <- grep("[Ss]td", colnames(xdata))
-xdata2 <- xdata[ , c(meanCol, stdCol)]
+# Merge the train and test datasets.
+traindata <- cbind(subjecttrain, ytrain, xtrain)
+testdata <- cbind(subjecttest, ytest, xtest)
+data <- rbind(testdata, traindata)
+
+# Removing characters that are illegal in R names
+colnames(data) <- gsub('\\(|\\)',"",names(data), perl = TRUE)
+colnames(data) <- gsub('\\-',"",names(data), perl = TRUE)
+colnames(data) <- gsub('\\,',"",names(data), perl = TRUE)
+
+
+
+# Extracts only columns in data containing measurements on the mean and standard deviation.
+meanCol <- grep("[Mm]ean", colnames(data))
+stdCol <- grep("[Ss]td", colnames(data))
+data2 <- data[ , c(1,2, meanCol, stdCol)]
 
 
 # Read in activity labels into R.
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # Set descriptive names for all columns in activities dataset.
-colnames(activities) <- c("act_label", "Activity")
+colnames(activities) <- c("Activity_label", "Activity")
 
-# Merge activities dataset with y dataset.
-ydata2 <- merge(ydata, activities)
-
-# Merge the xdata, ydata2 and suject datasets.
-data1 <- cbind(xdata2, ydata2["Activity"], subject)
+# Merge activities dataset with  data2 dataset.
+data2 <- merge(activities, data2)
 
 
 # Obtaining the average of each variable for each activity and each subject.
-DT <- data.table(data1)
-data2 <- DT[, lapply(.SD, mean), by = c("Activity", "Subject")]
-data2 <- data2[order(data2$Activity),]
+data3 <- ddply(melt(data2, id.vars = c( "Activity", 'Subject')), .(Subject, Activity),summarise, MeanSamples= mean(value))
 
 
-#Export data into a .txt file:
-write.table(data2, "tidydata.txt", sep = "\t")
+#Export data into a .txt file.
+write.table(data3, "tidydata.txt", sep = "\t")
 
